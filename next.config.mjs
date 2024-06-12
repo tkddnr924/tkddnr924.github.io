dfb66d8f844de3a312469fcda356f696cc2bf206
tkddnr924 <tkddnr924@naver.com>
2024-06-13 02:40:12 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const prefix = process.env.NODE_ENV === 'production' ? 'https://tkddnr924.github.io/' : ''
 
 const nextConfig = {
-  // output: 'export',
-  // assetPrefix: prefix,
+  output: 'export',
+  assetPrefix: prefix,
   reactStrictMode: false,
 };
 
