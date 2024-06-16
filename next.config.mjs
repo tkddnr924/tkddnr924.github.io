b568856e51de31f8f3f36a3209528d0e8b9cf215
tkddnr924 <tkddnr924@naver.com>
2024-06-16 11:34:57 +0900

    qwer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const prefix = process.env.NODE_ENV === 'production' ? 'https://tkddnr924.github.io/' : ''
+// const prefix = process.env.NODE_ENV === 'production' ? 'https://tkddnr924.github.io/' : ''
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: prefix,
   reactStrictMode: true,
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
